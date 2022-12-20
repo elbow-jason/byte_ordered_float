a6ba720e25dfdceaa8cfb616d34d69893e4577f3
Jason Goldberger <jasongoldberger@gmail.com>
2022-12-20 14:28:28 -0600

    add license

--- mix.exs
+++ mix.exs
@@ -25,4 +25,20 @@ defmodule ByteOrderedFloat.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
+
+  defp description() do
+    "A few sentences (a paragraph) describing the project."
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "byte_ordered_float",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/elixir-ecto/postgrex"}
+    ]
+  end
 end
