8de89180fc954c4a56f8055a8b0e56045b51afdc
Jason Goldberger <jasongoldberger@gmail.com>
2022-12-20 14:58:59 -0600

    add ex_doc to deps

--- mix.exs
+++ mix.exs
@@ -24,7 +24,9 @@ defmodule ByteOrderedFloat.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
   end
 
   defp description() do
